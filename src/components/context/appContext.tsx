f5a34f716080dbdde71fe470905577f5dad0b1fe
Ajay Titus <ajaytitus1386@gmail.com>
2023-08-24 12:00:10 +0530

    chore: add app context

---
+++ src/components/context/appContext.tsx
@@ -0,0 +1,15 @@
+import { ReactNode, createContext, useContext } from "react";
+
+const AppContext = createContext({});
+
+export const AppProvider = ({ children }: {children: ReactNode}) => { 
+    return (
+        <AppContext.Provider value={{}}>
+        {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+}
