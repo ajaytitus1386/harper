3bf7215da8d0020ce9566769c283a9fdb53f3032
Ajay Titus <ajaytitus1386@gmail.com>
2023-09-04 17:26:20 +0530

    style: auth background

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,9 @@ import React from "react"
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex items-center justify-center h-full">{children}</div>
+    <div className="flex items-center justify-center h-full px-4 py-8 bg-gradient-to-r from-landing-from to-landing-to">
+      {children}
+    </div>
   )
 }
 
