1af7eb3b638a8a8caff3a2efa4ff2a576d7d8580
Ajay Titus <ajaytitus1386@gmail.com>
2023-09-07 17:51:38 +0530

    chore: add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ import {
   faUpRightFromSquare,
 } from "@fortawesome/free-solid-svg-icons"
 
-import { faComments } from "@fortawesome/free-regular-svg-icons"
+import { faComments, faCopy } from "@fortawesome/free-regular-svg-icons"
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -34,7 +34,8 @@ library.add(
   faGear,
   faArrowRight,
   faPaperPlane,
-  faUpRightFromSquare
+  faUpRightFromSquare,
+  faCopy
 )
 
 export default function RootLayout({
