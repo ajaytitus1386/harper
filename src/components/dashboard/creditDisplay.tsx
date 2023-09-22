479dc6271347b2ae71437272436a884187f256bc
Ajay Titus <ajaytitus1386@gmail.com>
2023-09-22 13:57:08 +0530

    feat: credit display tooltip

--- src/components/dashboard/creditDisplay.tsx
+++ src/components/dashboard/creditDisplay.tsx
@@ -7,6 +7,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCoins } from "@fortawesome/free-solid-svg-icons"
 import { useTransactionModal } from "@/hooks/useTransactionModal"
 import useUserCredits, { useFetchUserCredits } from "@/hooks/useUserCredits"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip"
 
 const CreditDisplay = () => {
   const { userCredits, isFreeTier } = useUserCredits()
@@ -20,49 +26,60 @@ const CreditDisplay = () => {
     : 0
 
   return (
-    <Card className="flex flex-col space-y-2 bg-primary-100 px-4 py-2 rounded-md border-0">
-      <CardContent className="flex flex-row justify-between items-center p-0">
-        <div className="relative h-4 w-full overflow-hidden rounded-2xl bg-white">
-          <div
-            id="progress-bar"
-            style={{ backgroundSize: `${progress}% 100%` }}
-            className="relative w-full h-full bg-gradient-to-r from-upgrade-from to-upgrade-to bg-no-repeat"
-          >
-            <div
-              id="underlying-text"
-              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm font-bold tracking-wide text-upgrade-to invert mix-blend-difference"
-            >
-              {userCredits?.usedCredits}/{userCredits?.totalCredits}
-              <FontAwesomeIcon icon={faCoins} className="text-sm ml-1" />
-            </div>
-          </div>
-          <div
-            id="overlying-bar"
-            style={{
-              clipPath: `polygon(0 0, ${progress}% 0, ${progress}% 100%, 0% 100%)`,
-            }}
-            className="absolute top-0 left-0 w-full h-full"
-          >
-            <div
-              id="overlying-text"
-              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm font-bold tracking-wide text-white"
-            >
-              {userCredits?.usedCredits}/{userCredits?.totalCredits}
-              <FontAwesomeIcon icon={faCoins} className="text-sm ml-1" />
-            </div>
-          </div>
-        </div>
-      </CardContent>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Card className="flex flex-col space-y-2 bg-primary-100 px-4 py-2 rounded-md border-0">
+            <CardContent className="flex flex-row justify-between items-center p-0">
+              <div className="relative h-4 w-full overflow-hidden rounded-2xl bg-white">
+                <div
+                  id="progress-bar"
+                  style={{ backgroundSize: `${progress}% 100%` }}
+                  className="relative w-full h-full bg-gradient-to-r from-upgrade-from to-upgrade-to bg-no-repeat"
+                >
+                  <div
+                    id="underlying-text"
+                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm font-bold tracking-wide text-upgrade-to invert mix-blend-difference"
+                  >
+                    {userCredits?.usedCredits}/{userCredits?.totalCredits}
+                    <FontAwesomeIcon icon={faCoins} className="text-sm ml-1" />
+                  </div>
+                </div>
+                <div
+                  id="overlying-bar"
+                  style={{
+                    clipPath: `polygon(0 0, ${progress}% 0, ${progress}% 100%, 0% 100%)`,
+                  }}
+                  className="absolute top-0 left-0 w-full h-full"
+                >
+                  <div
+                    id="overlying-text"
+                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm font-bold tracking-wide text-white"
+                  >
+                    {userCredits?.usedCredits}/{userCredits?.totalCredits}
+                    <FontAwesomeIcon icon={faCoins} className="text-sm ml-1" />
+                  </div>
+                </div>
+              </div>
+            </CardContent>
 
-      <CardFooter className="p-0">
-        <Button
-          onClick={openModal}
-          className="w-full text-white font-bold px-2 py-1 bg-gradient-to-r from-upgrade-from to-upgrade-to bg-[length:200%_200%] bg-center hover:bg-right transition-all duration-300"
-        >
-          {isFreeTier ? "Upgrade" : "Add"}
-        </Button>
-      </CardFooter>
-    </Card>
+            <CardFooter className="p-0">
+              <Button
+                onClick={openModal}
+                className="w-full text-white font-bold px-2 py-1 bg-gradient-to-r from-upgrade-from to-upgrade-to bg-[length:200%_200%] bg-center hover:bg-right transition-all duration-300"
+              >
+                {isFreeTier ? "Upgrade" : "Add"}
+              </Button>
+            </CardFooter>
+          </Card>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p className="text-sm">{`Remaining Credits: ${
+            userCredits?.totalCredits - userCredits?.usedCredits
+          }`}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
 
